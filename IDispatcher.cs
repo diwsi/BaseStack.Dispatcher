14fff8158ca3411c979c2ec13bd121dd03255c30
diwsi <xenamorphx@gmail.com>
2023-02-19 19:48:10 +0300

    initial

---
+++ IDispatcher.cs
@@ -0,0 +1,12 @@
+﻿namespace Dispatcher
+{
+
+    public interface IDispatcher
+    {
+
+        Task<object?> Send(object request, CancellationToken cancellationToken = default);
+
+        Task<T?> Send<T>(object request, CancellationToken cancellationToken = default) where T : class;
+
+    }
+}
